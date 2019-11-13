dd90187a33455f82507301b079119facde0493da
Jake Hershey <joehershey45@gmail.com>
2019-11-13 07:08:30 -0800

    Finished mimic_dict and print_mimic

--- mimic.py
+++ mimic.py
@@ -49,14 +49,8 @@ import sys
 
 def mimic_dict(filename):
     """Returns mimic dict mapping each word to list of words which follow it."""
-    mimic_dictionary = {}
-    with open(filename, 'r') as f:
-        f_split = f.read().split()
-    f_split.insert(0, "")
-    for word in range(len(f_split) - 1):
-        mimic_dictionary[f_split[word]] = mimic_dictionary.get(
-            f_split[word], list()) + [f_split[word + 1]]
-    return mimic_dictionary
+    # +++your code here+++
+    raise NotImplementedError("Get to Work!")
 
 
 def print_mimic(mimic_dict, word):
@@ -67,15 +61,14 @@ def print_mimic(mimic_dict, word):
         - Randomly select a new seed word from this word list
         - Repeat this process 200 times
     """
-    for number in range(200):
-        print(word)
-        word = random.choice(mimic_dict[word])
+    # +++your code here+++
+    raise NotImplementedError("Get to Work!")
 
 
 # Provided main(), calls mimic_dict() and mimic()
 def main():
     if len(sys.argv) != 2:
-        print('usage: python mimic.py file-to-read')
+        print 'usage: python mimic.py file-to-read'
         sys.exit(1)
 
     d = mimic_dict(sys.argv[1])
